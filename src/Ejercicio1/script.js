a33285aa634a70096add4cdd4f69de8984ca1eeb
1603b3cla <calvoe53@gmail.com>
2024-03-13 11:51:10 -0600

    Correcciones

--- src/Ejercicio1/script.js
+++ src/Ejercicio1/script.js
@@ -1,17 +1,31 @@
+class Triangulo {
+    constructor(side1, side2, side3) {
+        this.lado1 = side1;
+        this.lado2 = side2;
+        this.lado3 = side3;
+    }
+
+    determinarTipoTriangulo() {
+        if (this.lado1 === this.lado2 && this.lado2 === this.lado3) {
+            this.mostrarResultado("Es un triángulo equilátero.");
+        } else if (this.lado1 === this.lado2 || this.lado2 === this.lado3 || this.lado1 === this.lado3) {
+            this.mostrarResultado("Es un triángulo isósceles.");
+        } else {
+            this.mostrarResultado("Es un triángulo escaleno.");
+        }
+    }
+
+    mostrarResultado(resultado) {
+        document.getElementById('resultado').innerText = resultado;
+    }
+}
+
+// Uso de la clase
 function determinarTipoTriangulo() {
     var lado1 = parseInt(document.getElementById('side1').value);
     var lado2 = parseInt(document.getElementById('side2').value);
     var lado3 = parseInt(document.getElementById('side3').value);
 
-    if (lado1 === lado2 && lado2 === lado3) {
-        mostrarResultado("Es un triángulo equilátero.");
-    } else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
-        mostrarResultado("Es un triángulo isósceles.");
-    } else {
-        mostrarResultado("Es un triángulo escaleno.");
-    }
-}
-
-function mostrarResultado(resultado) {
-    document.getElementById('resultado').innerText = resultado;
+    var triangulo = new Triangulo(lado1, lado2, lado3);
+    triangulo.determinarTipoTriangulo();
 }
